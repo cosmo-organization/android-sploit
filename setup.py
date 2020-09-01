c42516e6447e47f37761abb9a4b24acf08c98885
Sonu Aryan <sonuaryan76448002@gmail.com>
2020-09-01 07:38:55 +0530

    changes to launch

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="android-sploit-YOUR-USERNAME-HERE", # Replace with your own username
+    version="1.0",
+    author="Sonu Aryan",
+    author_email="sonuaryan76448002@gmail.com",
+    description="A package with adb command sets",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/cosmo-organization/android-sploit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
